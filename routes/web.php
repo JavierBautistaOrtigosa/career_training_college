fc38e3cd739bce17e6ef2a367ac2aae8e1d0c64d
Javier Bautista Ortigosa <dsng.jbo@gmail.com>
2026-02-25 14:38:32 +0800

    Resolve merge conflict in routes/web.php after restoring auth branch

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 // Default:
 
 Route::get('/', function () {
       return view('welcome');
-});
 
-// New:
 
-use App\Http\Controllers\AuthController;
 
-// Login page
-Route::get('/login', [AuthController::class, 'loginForm'])->name('login');
+      // Login page
+      Route::get('/login', [AuthController::class, 'loginForm'])->name('login');
 
-// Login form submission
-Route::post('/login', [AuthController::class, 'login']);
+      // Login form submission
+      Route::post('/login', [AuthController::class, 'login']);
 
-// Logout
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+      // Logout
+      Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
-// Home page (protected manually)
-Route::get('/home', function () {
-      return view('home'); // we will create this later
+      // Home page (protected manually)
+      Route::get('/home', function () {
+            return view('home'); // we will create this later
+      });
 });
