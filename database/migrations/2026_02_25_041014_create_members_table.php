bb764022fc57971fad638b005e3a2a56c8d03292
Javier Bautista Ortigosa <dsng.jbo@gmail.com>
2026-02-25 12:18:03 +0800

    Created users, members, and events migrations

--- database/migrations/2026_02_25_041014_create_members_table.php
+++ database/migrations/2026_02_25_041014_create_members_table.php
@@ -11,20 +11,24 @@ return new class extends Migration
        */
       public function up(): void
       {
-            Schema::create('users', function (Blueprint $table) {
+            Schema::create('members', function (Blueprint $table) {
                   $table->id();
-                  $table->string('name');
-                  $table->string('email')->unique();
-                  $table->string('password');
+                  $table->string('first_name');
+                  $table->string('last_name');
+                  $table->integer('age');
+                  $table->string('email');
+                  $table->string('phone');
+                  $table->string('address');
                   $table->timestamps();
             });
       }
+
 
       /**
        * Reverse the migrations.
        */
       public function down(): void
       {
-            Schema::dropIfExists('users');
+            Schema::dropIfExists('members');
       }
 };
--- database/migrations/2026_02_25_041127_create_events_table.php
+++ database/migrations/2026_02_25_041127_create_events_table.php
@@ -11,20 +11,23 @@ return new class extends Migration
        */
       public function up(): void
       {
-            Schema::create('users', function (Blueprint $table) {
+            Schema::create('events', function (Blueprint $table) {
                   $table->id();
-                  $table->string('name');
-                  $table->string('email')->unique();
-                  $table->string('password');
+                  $table->string('title');
+                  $table->dateTime('date_time'); // IMPORTANT: single column
+                  $table->string('location');
+                  $table->text('description')->nullable();
+                  $table->string('category');
                   $table->timestamps();
             });
       }
+
 
       /**
        * Reverse the migrations.
        */
       public function down(): void
       {
-            Schema::dropIfExists('users');
+            Schema::dropIfExists('events');
       }
 };
