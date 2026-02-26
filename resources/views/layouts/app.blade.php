cda665872a0acc05a0ff0af16b2ba0e5a47e03df
Javier Bautista Ortigosa <dsng.jbo@gmail.com>
2026-02-26 13:05:43 +0800

    Add XAMPP-safe navigation bar using named routes and url() helper

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,42 @@
 </head>
 
 <body>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+
+                  <a class="navbar-brand" href="{{ url('/home') }}">Career Training College</a>
+
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                        <span class="navbar-toggler-icon"></span>
+                  </button>
+
+                  <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav me-auto">
+
+                              <li class="nav-item">
+                                    <a class="nav-link" href="{{ url('/home') }}">Home</a>
+                              </li>
+
+                              <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('members.index') }}">Members</a>
+                              </li>
+
+                              <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('events.index') }}">Events</a>
+                              </li>
+
+                        </ul>
+
+                        <ul class="navbar-nav">
+                              <li class="nav-item">
+                                    <a class="nav-link text-danger" href="{{ route('logout') }}">Logout</a>
+                              </li>
+                        </ul>
+
+                  </div>
+            </div>
+      </nav>
+
 
       @yield('content')
 
